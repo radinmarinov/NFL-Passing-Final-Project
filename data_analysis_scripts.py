ddda62f04a15b65868dd3ffb3836701ee9b81d4c
radinmarinov <radinmarinov2021@u.northwestern.edu>
2020-11-14 14:10:37 -0600

    some data cleaning and feature engineering

--- data_analysis_scripts.py
+++ data_analysis_scripts.py
@@ -6,12 +6,32 @@ Created on Thu Nov 12 14:22:44 2020
 """
 
 import pandas as pd
+
+# Import data
 plays = pd.read_csv("plays.csv")
 
+###############
 # Data cleaning
-nulls = [{col, sum(pd.isnull(plays[col]))} for col in plays.columns]
-# null yardlineSide means LOS is at midfield
-# null offenseFormation should be removed from the data
-# null personnelO means offsetting penalties, remove
-# null defendersInTheBox means special teams pass or penalty
-# null numberOfPassRushers means penalty
+###############
+
+# Remove plays with penalties and plays with no offense formation
+plays = plays[(pd.isna(plays.penaltyCodes)) & \
+              (plays.playType != 'play_type_unknown') & \
+              (~pd.isna(plays.offenseFormation))]
+#check_nulls = [{col, sum(pd.isna(plays[col]))} for col in plays.columns]
+
+# 5 entries missing partial data (typeDropback, scores, game clock, abs yardline)
+#plays[pd.isna(plays.typeDropback)]
+
+#####################
+# Feature Engineering
+#####################
+
+# long (>=7 yards on a down)
+plays['longYardsToGo'] = plays.yardsToGo >= 7
+
+# Redzone (absoluteYardlineNumber <= 20)
+plays['redzone'] = plays.absoluteYardlineNumber <= 20
+
+# Incomplete pass
+plays['incompletePass'] = (plays.playDescription.str.contains("incomplete")) & (plays.playResult == 0)
