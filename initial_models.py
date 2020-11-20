0ba8b435bae0086eb82875425901fc5c41ac4bde
Lazar Gueorguiev <lazargueorguiev@gmail.com>
2020-11-20 10:50:08 -0600

    added random forest function

--- initial_models.py
+++ initial_models.py
@@ -1,7 +1,9 @@
-import pandas as pd
 import pickle
-from sklearn.model_selection import train_test_split
+
+import pandas as pd
 from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
 
 # Splits feature matrix and targets into training and test sets
@@ -11,11 +13,21 @@ def set_split(features, targets, test_ratio):
 
 
 # Decision tree model
-def tree_model(features, targets):
-    clf = tree.DecisionTreeClassifier()
+def tree_model(features, targets, maximum_depth, target_identifier):
+    clf = tree.DecisionTreeClassifier(max_depth=maximum_depth)
     clf = clf.fit(features, targets)
 
-    pkl_filename = "models/decision_tree_model.pkl"
+    pkl_filename = "models/decision_tree_model_" + target_identifier + ".pkl"
+    with open(pkl_filename, 'wb') as file:
+        pickle.dump(clf, file)
+
+
+# Random forest model
+def random_forest_model(features, targets, maximum_depth, target_identifier):
+    clf = RandomForestClassifier(max_depth=maximum_depth)
+    clf.fit(features, targets)
+
+    pkl_filename = "models/random_forest_model_" + target_identifier + ".pkl"
     with open(pkl_filename, 'wb') as file:
         pickle.dump(clf, file)
 
